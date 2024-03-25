f04d6731947a9362a1d8e749773fcd8c1be0e5f9
quantleosnow <164850154+quantleosnow@users.noreply.github.com>
2024-03-25 15:59:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -22,8 +22,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '逸仙中学答题库', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '逸仙中学答题库', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://woliwal.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '"逸仙中学答题库：探索知识，激发智慧，引领未来！"', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
